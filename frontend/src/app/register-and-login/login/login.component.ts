55c02bc51184cfdccb7d4efd08defcaf69ff9aa1
Daniel Lynch <daniellynch.dev@gmail.com>
2022-10-26 01:00:26 +0200

    add quick login form

--- frontend/src/app/register-and-login/login/login.component.ts
+++ frontend/src/app/register-and-login/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms'; import { UserDTO } from 'src/app/DTO/UserDTO';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'login',
@@ -6,12 +13,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private usersService: UsersService,
+  ) {
+    this.loginForm = this.formBuilder.group({
+      // username: [
+      //   null,
+      //   [
+      //     Validators.required,
+      //     Validators.minLength(5),
+      //     Validators.maxLength(30),
+      //   ],
+      // ],
+      password: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(40),
+        ],
+      ],
+      email: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(50),
+        ],
+      ],
+    });
+  }
 
   ngOnInit(): void {
-    console.log("test");
+  }
 
+  login() {
+    if (this.formHasValidationErrors()) return;
+    // const username = this.loginForm.get('username')?.value;
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+
+    const user: UserDTO = {
+      // username: username,
+      password: password,
+      email: email,
+    };
+
+    // Send User and Admin Log to API with service
+    this.usersService.createUser(user);
+  }
+
+  formHasValidationErrors(): boolean {
+    let errorCount: number = 0;
+    Object.keys(this.loginForm.controls).forEach((key) => {
+      const controlErrors: ValidationErrors | null | undefined =
+        this.loginForm.get(key)?.errors;
+      if (controlErrors != null) {
+        // Form group has errors
+        Object.keys(controlErrors).forEach((keyError) => {
+          errorCount += 1;
+          // console.log(
+          //   'Key control: ' + key + ', keyError: ' + keyError + ', err value: ',
+          //   controlErrors[keyError]
+          // );
+          return true;
+        });
+      }
+    });
+    // Form is valid
+    return false;
   }
 
 }
