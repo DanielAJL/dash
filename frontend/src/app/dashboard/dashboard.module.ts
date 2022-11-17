be86b6469d81e90f9617c0ed316ae5e338fa8d58
Daniel Lynch <daniellynch.dev@gmail.com>
2022-11-17 21:39:59 +0100

    create child component for friends

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './dashboard.component';
+import { FriendsComponent } from './friends/friends.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, FriendsComponent],
   imports: [SharedModule, HttpClientModule],
 })
 export class DashboardModule { }
---
+++ frontend/src/app/dashboard/friends/friends.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FriendRequestDTO } from 'src/app/DTO/FriendRequestDTO';
+import { UserDTO } from 'src/app/DTO/UserDTO';
+
+class userAndRequestDataInOne {
+  user: UserDTO;
+  friendReq: FriendRequestDTO;
+};
+
+
+@Component({
+  selector: 'friends',
+  templateUrl: './friends.component.html',
+  styleUrls: ['./friends.component.scss']
+})
+
+export class FriendsComponent implements OnInit {
+
+  constructor() { }
+  @Input() friends: userAndRequestDataInOne[]; // decorate the property with @Input()
+
+  ngOnInit(): void {
+    console.log(this.friends);
+
+  }
+
+}
